299c3c1023e78ccbdde6da0591fc9ec3adc9496d
Vyacheslav Nozhenko <vv.nojenko@gmail.com>
2016-08-30 11:07:13 +0300

    ConfigurationFile example

---
+++ Lesson1/03-ConfigurationFile/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace _03_ConfigurationFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string conString = ConfigurationManager.ConnectionStrings["Shop"].ConnectionString;
+
+            SqlConnection connection = new SqlConnection(conString);
+            try
+            {
+                connection.Open();
+                Console.WriteLine("Connection success!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("[ERROR]: {0}", ex.Message);
+            }
+            finally
+            {
+                if (connection.State == System.Data.ConnectionState.Open)
+                    connection.Close();
+            }
+        }
+    }
+}
