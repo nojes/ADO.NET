d106b2d10310b8f3ba06ee9e0b2cc6dd88989248
Vyacheslav Nozhenko <vv.nojenko@gmail.com>
2016-08-30 10:21:44 +0300

    ConnectionString example

--- Lesson1/01-ConnectionString/Program.cs
+++ Lesson1/01-ConnectionString/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,29 @@ namespace _01_ConnectionString
     {
         static void Main(string[] args)
         {
+            SqlConnection connection = new SqlConnection();
+            connection.ConnectionString = @"
+                Data Source=localhost;
+                Initial Catalog = ShopAdo;
+                Integrated Security=False; 
+                user=sa;
+                password=student;
+                Connect Timeout=15;";
+            try
+            {
+                connection.Open();
+                Console.WriteLine("Connection success!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("[ERROR]: {0}", ex.Message);
+            }
+            finally
+            {
+                if(connection.State == System.Data.ConnectionState.Open) {
+                    connection.Close();
+                }
+            }
         }
     }
 }
