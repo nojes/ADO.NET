230b34b36181a30edb3836497fd605ea1d2de57e
Vyacheslav Nozhenko <vv.nojenko@gmail.com>
2016-09-01 11:55:47 +0300

    IEnumerable

---
+++ Lesson2-SqlCommand/IEnumerable/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+
+namespace IEnumerable_example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] array = new int[] { 1, 22, 58, 584, 1 };
+
+            Console.WriteLine("foreach");
+            foreach (var item in array)
+            {
+                Console.Write("{0} ", item);
+            }
+
+            Console.WriteLine("\nIEnumerator");
+            IEnumerable enumerable = array;
+            IEnumerator enumerator = enumerable.GetEnumerator();
+            enumerator.Reset();
+            while (enumerator.MoveNext())
+            {
+                Console.Write("{0} ", enumerator.Current);
+            }
+            Console.WriteLine();
+        }
+    }
+}
