1a4953feb985687814fa38b8278b9d7852289240
Vyacheslav Nozhenko <vv.nojenko@gmail.com>
2016-09-08 11:56:53 +0300

    'DataAdapter' WinForms example

---
+++ Lesson4/02-WinFormCategory/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace _02_WinFormCategory
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSave = new System.Windows.Forms.Button();
+            this.dgvCategory = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCategory)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(12, 240);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 1;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // dgvCategory
+            // 
+            this.dgvCategory.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvCategory.Location = new System.Drawing.Point(13, 13);
+            this.dgvCategory.Name = "dgvCategory";
+            this.dgvCategory.Size = new System.Drawing.Size(421, 209);
+            this.dgvCategory.TabIndex = 2;
+            this.dgvCategory.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvCategory_CellContentClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(446, 275);
+            this.Controls.Add(this.dgvCategory);
+            this.Controls.Add(this.btnSave);
+            this.Name = "Form1";
+            this.Text = "Category";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCategory)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.DataGridView dgvCategory;
+    }
+}
+
---
+++ Lesson4/02-WinFormCategory/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _02_WinFormCategory
+{
+    public partial class Form1 : Form
+    {
+        string conString = ConfigurationManager.ConnectionStrings["shop"].ConnectionString;
+        SqlConnection connection;
+        SqlDataAdapter adapter;
+        DataTable category;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            connection = new SqlConnection(conString);
+            adapter = new SqlDataAdapter("SELECT [CategoryId], [CategoryName] FROM dbo.Category", connection);
+            adapter.RowUpdated += Adapter_RowUpdated;
+            category = new DataTable("Category");
+
+            SqlCommandBuilder builder = new SqlCommandBuilder(adapter);
+
+            adapter.DeleteCommand = builder.GetDeleteCommand();
+            adapter.InsertCommand = builder.GetInsertCommand();
+            adapter.UpdateCommand = builder.GetUpdateCommand(true);
+
+            adapter.Fill(category);
+
+            dgvCategory.DataSource = category;
+        }
+
+        private void Adapter_RowUpdated(object sender, SqlRowUpdatedEventArgs e)
+        {
+            if (e.Errors == null && e.StatementType == StatementType.Insert)
+            {
+                SqlCommand cmd = new SqlCommand("SELECT CategoryId FROM dbo.Category WHERE CategoryId = IDENT_CURRENT('dbo.Category')", connection);
+
+                if (connection.State != ConnectionState.Open) { 
+                    connection.Open();
+                }   
+                int count = (int)cmd.ExecuteScalar();
+                e.Row["CategoryId"] = count;
+
+            }
+        }
+
+        private void dgvCategory_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            adapter.Update(category);
+            MessageBox.Show("Updated!", "Database", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
