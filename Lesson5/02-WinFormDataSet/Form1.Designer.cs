8caf15e9a54de992b747f10e494c00a996b47481
Vyacheslav Nozhenko <vv.nojenko@gmail.com>
2016-09-13 11:58:44 +0300

    WinForm DataSet example

---
+++ Lesson5/02-WinFormDataSet/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace _02_WinFormDataSet
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvRegions = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dgvCities = new System.Windows.Forms.DataGridView();
+            this.btnSave = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvRegions)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCities)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvRegions
+            // 
+            this.dgvRegions.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvRegions.Location = new System.Drawing.Point(12, 29);
+            this.dgvRegions.Name = "dgvRegions";
+            this.dgvRegions.Size = new System.Drawing.Size(674, 199);
+            this.dgvRegions.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Regions";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 241);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Cities";
+            // 
+            // dgvCities
+            // 
+            this.dgvCities.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvCities.Location = new System.Drawing.Point(12, 257);
+            this.dgvCities.Name = "dgvCities";
+            this.dgvCities.Size = new System.Drawing.Size(674, 188);
+            this.dgvCities.TabIndex = 2;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(12, 457);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 4;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(698, 492);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dgvCities);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dgvRegions);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvRegions)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCities)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvRegions;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dgvCities;
+        private System.Windows.Forms.Button btnSave;
+    }
+}
+
---
+++ Lesson5/02-WinFormDataSet/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _02_WinFormDataSet
+{
+    public partial class Form1 : Form
+    {
+        string conString = ConfigurationManager.ConnectionStrings["Region"].ConnectionString;
+        SqlConnection connect = new SqlConnection();
+        DataSet ds = new DataSet();
+
+        DataTable region = new DataTable("Region");
+        DataTable city = new DataTable("City");
+
+        SqlDataAdapter regionAdapter;
+        SqlDataAdapter cityAdapter;
+
+        BindingSource regionBinding = new BindingSource();
+        BindingSource cityBinding = new BindingSource();
+
+        private void InitSchema()
+        {
+            connect.ConnectionString = conString;
+
+            regionAdapter = new SqlDataAdapter("SELECT * FROM dbo.Region", connect);
+            regionAdapter.FillSchema(region, SchemaType.Mapped);
+            SqlCommandBuilder cRegion = new SqlCommandBuilder(regionAdapter);
+            regionAdapter.InsertCommand = cRegion.GetInsertCommand();
+            regionAdapter.UpdateCommand = cRegion.GetUpdateCommand();
+            regionAdapter.DeleteCommand = cRegion.GetDeleteCommand();
+
+            cityAdapter = new SqlDataAdapter("SELECT * FROM dbo.City", connect);
+            cityAdapter.FillSchema(city, SchemaType.Mapped);
+            SqlCommandBuilder cCity = new SqlCommandBuilder(cityAdapter);
+            cityAdapter.InsertCommand = cCity.GetInsertCommand();
+            cityAdapter.UpdateCommand = cCity.GetUpdateCommand();
+            cityAdapter.DeleteCommand = cCity.GetDeleteCommand();
+
+            ds.Tables.Add(region);
+            ds.Tables.Add(city);
+
+            DataRelation relation = new DataRelation("RegionCity",
+                ds.Tables[0].Columns["RegionId"],
+                ds.Tables[1].Columns["RegionId"]
+            );
+
+            ForeignKeyConstraint fKey = new ForeignKeyConstraint("CityForeignKey", 
+                ds.Tables[0].Columns["RegionId"],
+                ds.Tables[1].Columns["RegionId"]
+            );
+            fKey.DeleteRule = Rule.Cascade;
+            fKey.UpdateRule = Rule.Cascade;
+            ds.Tables[1].Constraints.Add(fKey);
+            ds.EnforceConstraints = true;
+            ds.Relations.Add(relation);
+
+
+            ds.Tables[0].PrimaryKey = new DataColumn[] { ds.Tables[0].Columns["RegionId"] };
+            ds.Tables[0].Columns["RegionId"].AutoIncrement = true;
+            ds.Tables[0].Columns["RegionId"].Unique = true;
+            ds.Tables[0].Columns["RegionId"].AutoIncrementStep = -1;
+            ds.Tables[0].Columns["RegionId"].AutoIncrementSeed = -1;
+
+            ds.Tables[1].PrimaryKey = new DataColumn[] { ds.Tables[1].Columns["CityId"] };
+            ds.Tables[1].Columns["CityId"].AutoIncrement = true;
+            ds.Tables[1].Columns["CityId"].Unique = true;
+            ds.Tables[1].Columns["CityId"].AutoIncrementStep = -1;
+            ds.Tables[1].Columns["CityId"].AutoIncrementSeed = -1;
+        }
+
+        private void FormBinding()
+        {
+            regionBinding.DataSource = ds;
+            regionBinding.DataMember = "Region";
+            dgvRegions.DataSource = regionBinding;
+
+            cityBinding.DataSource = regionBinding;
+            cityBinding.DataMember = "RegionCity";
+            dgvCities.DataSource = cityBinding;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            InitSchema();
+            regionAdapter.Fill(region);
+            cityAdapter.Fill(city);
+            FormBinding();
+
+            dgvCities.UserDeletingRow += DgvCities_UserDeletingRow;
+            regionAdapter.RowUpdated += RegionAdapter_RowUpdated;
+        }
+
+        private void RegionAdapter_RowUpdated(object sender, SqlRowUpdatedEventArgs e)
+        {
+            if (e.Errors == null && e.StatementType == StatementType.Insert)
+            {
+                SqlCommand cmd = new SqlCommand("SELECT RegionId FROM Region WHERE RegionId = IDENT_CURRENT('dbo.Region')", connect);
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    e.Row["RegionId"] = reader["RegionId"];
+                }
+                reader.Close();
+
+            }
+        }
+
+        private void DgvCities_UserDeletingRow(object sender, DataGridViewRowCancelEventArgs e)
+        {
+            e.Cancel = true;
+            DialogResult res = MessageBox.Show(" You remove the record?", "Deleting", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (res == DialogResult.Yes)
+            {
+                foreach (DataGridViewRow r in this.dgvCities.SelectedRows)
+                {
+                    dgvCities.Rows.RemoveAt(r.Index);
+                }
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            regionAdapter.Update(ds.Tables[0].Select(null, null, DataViewRowState.Added));
+            cityAdapter.Update(ds.Tables[1].Select(null, null, DataViewRowState.Added));
+
+            cityAdapter.Update(ds.Tables[1].Select(null, null, DataViewRowState.Deleted));
+        }
+    }
+}
