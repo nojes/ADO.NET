ed7f67fc9e2d35ed39f89ad781f727c145179365
Vyacheslav Nozhenko <vv.nojenko@gmail.com>
2016-09-15 10:54:05 +0300

    linq to array example

--- Lesson6/01-linqToArray/Program.cs
+++ Lesson6/01-linqToArray/Program.cs
@@ -10,6 +10,14 @@ namespace _01_linqToArray
     {
         static void Main(string[] args)
         {
+            int[] arr1 = new int[] { 1, 4, 5, 79, 24 };
+            int[] arr2 = new int[] { 5, 3, 56, -13, 74 };
+
+            var query = arr1.Where(p => p > arr2.Average()).ToArray();
+
+            foreach (var item in query) {
+                Console.WriteLine(item + " ");
+            }
         }
     }
 }
