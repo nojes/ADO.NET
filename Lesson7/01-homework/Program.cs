2b808217dfe7cc5a8da630505e9cc57baedf4eea
Vyacheslav Nozhenko <vv.nojenko@gmail.com>
2016-09-20 09:58:19 +0300

    queries example

--- Lesson7/01-homework/Program.cs
+++ Lesson7/01-homework/Program.cs
@@ -10,6 +10,31 @@ namespace _01_homework
     {
         static void Main(string[] args)
         {
+            string[] cars = { "Alfa Romeo", "BMW", "Aston Martin", "Audi", "Mersedes Benz", "Chevrolet", "Subaru", "Ford"};
+
+            var q1 = cars.Where(car => { return car.Length == 4; });
+            var q1_2 = from car in cars
+                       where car.Length == 4
+                       select car;
+            var q2 = cars.Where(car => car.Count(x => x == 'o') == 2);
+            var q3 = cars.Where(car => car.Length < 5).Select(car => new { car, car.Length });
+
+            writeItems("Cars, where mark length = 4", q1);
+            writeItems("Cars, where mark length = 4 (2)", q1_2);
+            writeItems("Cars, where mark contain 2 'o'", q2);
+            writeItems("Anonym objects", q3);
+        }
+
+        static void writeItems<T>(string title, IEnumerable<T> items)
+        {
+            ConsoleColor currentColor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("{0}:", title);
+            Console.ForegroundColor = currentColor;
+
+            foreach (var item in items) {
+                Console.WriteLine("\t{0}", item);
+            }
         }
     }
 }
