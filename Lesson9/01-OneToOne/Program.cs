84c44d45d58e463a9b1bbb65457590837d004840
Vyacheslav Nozhenko <vv.nojenko@gmail.com>
2016-09-29 09:25:33 +0300

    lesson9

---
+++ Lesson9/01-OneToOne/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataLayer;
+using DataLayer.DbLayer;
+
+using System.Data.Entity.Migrations;
+
+namespace _01_OneToOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Use
+        }
+    }
+}
